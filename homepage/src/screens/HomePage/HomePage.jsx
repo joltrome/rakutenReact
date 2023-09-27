8a1029b37115ba6eb2039d3a3931690279f9b4a8
drdracool <97832811+drdracool@users.noreply.github.com>
2023-09-27 12:48:18 +0900

    homepage & product exported code using Anima

---
+++ homepage/src/screens/HomePage/HomePage.jsx
@@ -0,0 +1,229 @@
+import React from "react";
+import "./style.css";
+
+export const HomePage = () => {
+  return (
+    <div className="home-page">
+      <div className="div">
+        <div className="overlap">
+          <div className="text-input" />
+          <div className="text-input-container">
+            <div className="overlap-group">
+              <div className="overlap-2">
+                <div className="BEYOURBEST-strip">BE YOUR BEST</div>
+                <div className="text-input-2" />
+              </div>
+              <div className="overlap-3">
+                <div className="BEYOURBEST-strip">BE YOUR BEST</div>
+                <div className="text-input-3" />
+              </div>
+              <div className="text-wrapper">BE YOUR BEST</div>
+              <div className="BEYOURBEST-strip-2">BE YOUR BEST</div>
+              <div className="BEYOURBEST-strip-3">BE YOUR BEST</div>
+              <div className="BEYOURBEST-strip-4">BE YOUR BEST</div>
+              <div className="overlap-4">
+                <div className="test">BE YOUR BEST</div>
+                <div className="text-input-4" />
+              </div>
+              <div className="text-input-5" />
+              <div className="text-input-6" />
+              <div className="text-input-7" />
+              <div className="text-input-8" />
+            </div>
+          </div>
+          <img className="image" alt="Image" src="/img/image-4.png" />
+          <div className="text-input-9">
+            <button className="button">
+              <div className="enabled">New Arrival</div>
+            </button>
+            <button className="enabled-wrapper">
+              <div className="enabled">Trending Now</div>
+            </button>
+            <button className="button-2">
+              <div className="text-wrapper-2">Regions</div>
+              <img className="vector" alt="Vector" src="/img/vector.svg" />
+            </button>
+            <button className="div-wrapper">
+              <div className="enabled">Essentials</div>
+            </button>
+          </div>
+        </div>
+        <div className="overlap-5">
+          <img className="img" alt="Image" src="/img/image.png" />
+          <p className="p">
+            <span className="span">
+              Corporate Info
+              <br />
+            </span>
+            <span className="text-wrapper-3">
+              Career
+              <br />
+            </span>
+            <span className="span">
+              Press
+              <br />
+              Investor relations
+              <br />
+              Corporate governance
+            </span>
+          </p>
+          <p className="div-2">
+            <span className="span">
+              Help
+              <br />
+            </span>
+            <span className="text-wrapper-3">
+              Customer Service
+              <br />
+            </span>
+            <span className="span">
+              Find a store
+              <br />
+              Legal &amp; Privacy
+              <br />
+              Contact
+              <br />
+              Report a scam
+              <br />
+              Cookie Settings
+            </span>
+          </p>
+          <p className="div-3">
+            <span className="span">
+              Shop
+              <br />
+            </span>
+            <span className="text-wrapper-3">
+              Ladies
+              <br />
+            </span>
+            <span className="span">
+              Men
+              <br />
+              Kids
+              <br />
+              Home
+              <br />
+              Sport
+              <br />
+              <br />
+              <br />
+            </span>
+          </p>
+          <p className="div-4">
+            <span className="span">
+              Sign up now and be the first to know about exclusive offers, latest fashion news &amp; style tips!
+              <br />
+              <br />
+            </span>
+            <span className="text-wrapper-4">Read more</span>
+          </p>
+        </div>
+        <div className="overlap-6">
+          <div className="tooltip">
+            <div className="overlap-7">
+              <div className="FORMALS-styles-wrapper">
+                <div className="text-wrapper-5">洋菓子</div>
+              </div>
+              <div className="CASUALS-styles-wrapper">
+                <div className="text-wrapper-5">その他</div>
+              </div>
+            </div>
+          </div>
+          <div className="streetstyle">Food</div>
+          <img className="image-2" alt="Image" src="/img/image-5.png" />
+          <div className="text-wrapper-6">和菓子</div>
+        </div>
+        <div className="horizontal-container">
+          <div className="overlap-8">
+            <div className="frame">
+              <div className="overlap-group-2">
+                <p className="text-wrapper-7">Super Fast and Free Delivery</p>
+                <img className="image-3" alt="Image" src="/img/image-11.png" />
+              </div>
+            </div>
+            <div className="container">
+              <div className="text-wrapper-8">Super Secure Payment System</div>
+              <img className="image-4" alt="Image" src="/img/image-10.png" />
+            </div>
+            <div className="text-input-container-2">
+              <div className="non-contactshipping">Non- Contact Shipping</div>
+              <img className="image-5" alt="Image" src="/img/image-9.png" />
+            </div>
+            <div className="button-3">
+              <div className="moneybackguranteed">Money Back Guranteed</div>
+              <img className="image-6" alt="Image" src="/img/image-8.png" />
+            </div>
+          </div>
+        </div>
+        <div className="container-2">
+          <div className="container-3">
+            <div className="container-4">
+              <img className="image-7" alt="Image" src="/img/image-7.png" />
+              <div className="overlap-group-3">
+                <div className="text-wrapper-9">Cargo Trousers</div>
+                <div className="rs">Rs 2,299.00</div>
+              </div>
+            </div>
+            <div className="switch">
+              <img className="image-7" alt="Image" src="/img/image-6.png" />
+              <div className="overlap-group-3">
+                <div className="text-wrapper-9">Corset Top</div>
+                <div className="rs">Rs 2,299.00</div>
+              </div>
+            </div>
+            <div className="container-5">
+              <img className="image-7" alt="Image" src="/img/image-3.png" />
+              <div className="overlap-group-3">
+                <div className="text-wrapper-9">Cargo Trousers</div>
+                <div className="rs">Rs 2,299.00</div>
+              </div>
+            </div>
+            <div className="container-6">
+              <img className="image-7" alt="Image" src="/img/image-2.png" />
+              <div className="overlap-group-3">
+                <div className="text-wrapper-9">Cargo Trousers</div>
+                <div className="rs">Rs 2,299.00</div>
+              </div>
+            </div>
+            <div className="card">
+              <img className="image-7" alt="Image" src="/img/image-1.png" />
+              <div className="overlap-group-3">
+                <div className="text-wrapper-9">Cargo Trousers</div>
+                <div className="rs">Rs 2,299.00</div>
+              </div>
+            </div>
+            <div className="text-input-container-3">
+              <div className="overlap-9">
+                <p className="text-wrapper-10">Want to see all New Arrivals?</p>
+              </div>
+              <div className="overlap-group-3">
+                <img
+                  className="productsqr"
+                  alt="Productsqr"
+                  src="/img/arrow1-31-productsqr-31-scroll-31-newarrivals-31-14.png"
+                />
+                <div className="viewall">View All</div>
+              </div>
+            </div>
+          </div>
+          <div className="newarrivals">Best Sellers</div>
+        </div>
+        <div className="text-input-container-4">
+          <div className="home-menu">Home</div>
+          <div className="findastore-menu">Find a store</div>
+          <div className="customerservice">Customer Service</div>
+          <div className="enabled-login-wrapper">
+            <div className="enabled-login">Log-In</div>
+          </div>
+          <button className="hovered-signup-wrapper">
+            <div className="hovered-signup">Sign-Up</div>
+          </button>
+          <div className="cart">
+            <img className="delivery-truck" alt="Delivery truck" src="/img/delivery-truck-1.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
