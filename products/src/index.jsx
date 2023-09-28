677630c18d6e52febe597d34c2c2ac4b015cc881
drdracool <97832811+drdracool@users.noreply.github.com>
2023-09-28 10:44:34 +0900

    Delete products directory

--- products/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOMClient from "react-dom/client";
-import { Products } from "./screens/Products";
-
-const app = document.getElementById("app");
-const root = ReactDOMClient.createRoot(app);
-root.render(<Products />);
